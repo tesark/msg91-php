a0c63d8f742df5008cba3d366df0f2fa6989e64f
Chandresh Rajkumar Manonmani <chan@tqube.com>
2017-08-03 19:18:45 +0530

    Travis Updated

---
+++ tests/MyFirstPackage/EmailTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace MyFirstPackage;
+
+use MyFirstPackage\Email;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
+
