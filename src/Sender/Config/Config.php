db268c6a0f4b6729910b4d2fff889a8dc28ae20b
venkatSkpi <venkatsamuthiram5@gmail.com>
2017-09-05 19:01:58 +0530

    Config file added and basic tested

--- src/Sender/Config/Config.php
+++ src/Sender/Config/Config.php
@@ -16,34 +16,68 @@ use Noodlehaus\AbstractConfig;
 class MyConfig extends AbstractConfig // class testing pending
 {
     protected $config;
+    protected $common;
+    protected $promotionalSms;
+    protected $transactionalSms;
+    protected $otp;
     public function __construct()
     {
         $config = new Config($_SERVER["DOCUMENT_ROOT"]. '/../config');
+        if (isset($config['msg91']) && $config['msg91']) {
+            if ($this->checkKey('common', $config['msg91'])) {
+                $this->common = $config['msg91']['common'];
+            }
+            if ($this->checkKey('promotionalSms', $config['msg91'])) {
+                $this->promotionalSms   = $config['msg91']['promotionalSms'];
+            }
+            if ($this->checkKey('transactionalSms', $config['msg91'])) {
+                $this->transactionalSms = $config['msg91']['transactionalSms'];
+            }
+            if ($this->checkKey('otp', $config['msg91'])) {
+                $this->otp    = $config['msg91']['otp'];
+            }
+        }
     }
     /*
-    *This function return Default and Env file Values
+    *This function return Default and Env file Values  
     *
     */
     protected function getDefaults()
     {
         return array(
             'common' => [
-                'country'      => getenv('COUNTRY')      ? (string) getenv('COUNTRY')      : 'IN',
-                'transAuthKey' => getenv('TRANSAUTHKEY') ? (string) getenv('TRANSAUTHKEY') : '170867ARdROqjKklk599a87a1',
-                'promoAuthKey' => getenv('PROMOAUTHKEY') ? (string) getenv('PROMOAUTHKEY') : '170867ARdROqjKklk599a87a1',
-                'otpAuthKey'   => getenv('OTPAUTHKEY')   ? (string) getenv('OTPAUTHKEY')   : '170436A8DCExM8m259969531',
+                'country'      => getenv('COUNTRY')      ? (string) getenv('COUNTRY')      : $this->checkKey('country', $this->common),
+                'transAuthKey' => getenv('TRANSAUTHKEY') ? (string) getenv('TRANSAUTHKEY') : $this->checkKey('transAuthKey', $this->common),
+                'promoAuthKey' => getenv('PROMOAUTHKEY') ? (string) getenv('PROMOAUTHKEY') : $this->checkKey('promoAuthKey', $this->common),
+                'otpAuthKey'   => getenv('OTPAUTHKEY')   ? (string) getenv('OTPAUTHKEY')   : $this->checkKey('otpAuthKey', $this->common),
             ],
             'promotionalSms' => [
-                'sender' => getenv('SENDER') ? (string) getenv('SENDER') : null,
+                'sender' => getenv('SENDER') ? (string) getenv('SENDER') : $this->checkKey('sender', $this->promotionalSms),
             ],
             'transactionalSms' => [
-                'sender' => getenv('SENDER') ? (string) getenv('SENDER'): null,
+                'sender' => getenv('SENDER') ? (string) getenv('SENDER') : $this->checkKey('sender', $this->transactionalSms),
             ],
             'otp' => [
-                'sender'     => getenv('SENDER')    ? (string) getenv('SENDER')    : null,
-                'otp_length' => getenv('OTPLENGTH') ? (string) getenv('OTPLENGTH') : 4, // Length min 4 max 9
-                'otp_expiry' => getenv('OTPEXPIRY') ? (string) getenv('OTPEXPIRY') : 1, // Minutes default 1 day
+                'sender'     => getenv('SENDER')    ? (string) getenv('SENDER')    : $this->checkKey('sender', $this->otp),
+                'otp_length' => getenv('OTPLENGTH') ? ( int ) getenv('OTPLENGTH')  : $this->checkKey('otp_length', $this->otp), // Length min 4 max 9
+                'otp_expiry' => getenv('OTPEXPIRY') ? ( int ) getenv('OTPEXPIRY')  : $this->checkKey('otp_expiry', $this->otp), // Minutes default 1 day
             ]
         );
+    }
+    /*
+    *this function check key present in aaray
+    *
+    */
+    protected function checkKey($key,$array)
+    {
+        if (isset($key) && $array) {
+            if (array_key_exists($key, $array)) {
+                return $array[$key];
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
